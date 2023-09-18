8ec5a7ae9b2c313419e3d384a70d3d3e9c62b16c
klupka <sethklupka123@gmail.com>
2023-09-18 18:38:22 -0500

    updated cards.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,19 @@
 import "../assets/Cards.css";
 
-function Card({ imageURL, year, brand, model }) {
+function Card({
+    imageURL,
+    year,
+    brand,
+    model,
+    mileage,
+    price,
+    location,
+    timeLeft,
+}) {
     const carName = `${year} ${brand} ${model}`;
     const bgImage = `background-image:url('${imageURL}')`;
+    const priceStr = `$${price}`;
+    const mileageStr = `${mileage} mi`;
     return (
         <div className="card">
             <div
@@ -10,7 +21,18 @@ function Card({ imageURL, year, brand, model }) {
                 style={{ backgroundImage: `url(${imageURL})` }}
                 alt={carName}
             ></div>
-            <div className="card-title">{carName}</div>
+            <div className="card-header">
+                <ul>
+                    <li>{carName}</li>
+                    <li>{priceStr}</li>
+                </ul>
+            </div>
+            <div className="card-details">
+                <ul>
+                    <li>{location}</li>
+                    <li>{timeLeft}</li>
+                </ul>
+            </div>
         </div>
     );
 }
@@ -23,18 +45,30 @@ const Cards = () => {
                 year="1998"
                 brand="Lexus"
                 model="IS 300"
+                mileage="10,590"
+                price="9,790"
+                location="San Antonio, TX 78154"
+                timeLeft="10h 53m"
             />
             <Card
                 imageURL="https://totallythatstupid.files.wordpress.com/2014/07/gw_640.jpg?w=640"
                 year="1998"
                 brand="Lexus"
                 model="IS 300"
+                mileage="10,590"
+                price="9,790"
+                location="San Antonio, TX 78154"
+                timeLeft="10h 53m"
             />
             <Card
                 imageURL="https://totallythatstupid.files.wordpress.com/2014/07/gw_640.jpg?w=640"
                 year="1998"
                 brand="Lexus"
                 model="IS 300"
+                mileage="10,590"
+                price="9,790"
+                location="San Antonio, TX 78154"
+                timeLeft="10h 53m"
             />
         </div>
     );
