ab761ca68cbf736ac2d4dc410b1ea534c1a365fc
klupka <sethklupka123@gmail.com>
2023-09-21 22:01:07 -0500

    updated cards x3.

---
+++ src/assets/sample_data.jsx
@@ -0,0 +1,112 @@
+export const cars = [
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
+        year: "1998",
+        brand: "Lexus",
+        model: "IS 300",
+        mileage: "10,590",
+        price: "9,790",
+        location: "San Antonio, TX 78154",
+        timeLeft: "10h 53m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
+        year: "2000",
+        brand: "Ford",
+        model: "F-350 Super Duty Lariat 4x4",
+        mileage: "87,700",
+        price: "12,500",
+        location: "Loveland, OH 45140",
+        timeLeft: "00h 1m",
+    },
+    {
+        imageURL:
+            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
+        year: "2017",
+        brand: "Ford",
+        model: "GT350 Fastback RWD",
+        mileage: "32,900",
+        price: "44,000",
+        location: "Fort Gratiot, MI 48059",
+        timeLeft: "23h 16m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
+        year: "1998",
+        brand: "Lexus",
+        model: "IS 300",
+        mileage: "10,590",
+        price: "9,790",
+        location: "San Antonio, TX 78154",
+        timeLeft: "10h 53m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
+        year: "2000",
+        brand: "Ford",
+        model: "F-350 Super Duty Lariat 4x4",
+        mileage: "87,700",
+        price: "12,500",
+        location: "Loveland, OH 45140",
+        timeLeft: "00h 1m",
+    },
+    {
+        imageURL:
+            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
+        year: "2017",
+        brand: "Ford",
+        model: "GT350 Fastback RWD",
+        mileage: "32,900",
+        price: "44,000",
+        location: "Fort Gratiot, MI 48059",
+        timeLeft: "23h 16m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
+        year: "1998",
+        brand: "Lexus",
+        model: "IS 300",
+        mileage: "10,590",
+        price: "9,790",
+        location: "San Antonio, TX 78154",
+        timeLeft: "10h 53m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
+        year: "2000",
+        brand: "Ford",
+        model: "F-350 Super Duty Lariat 4x4",
+        mileage: "87,700",
+        price: "12,500",
+        location: "Loveland, OH 45140",
+        timeLeft: "00h 1m",
+    },
+    {
+        imageURL:
+            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
+        year: "2017",
+        brand: "Ford",
+        model: "GT350 Fastback RWD",
+        mileage: "32,900",
+        price: "44,000",
+        location: "Fort Gratiot, MI 48059",
+        timeLeft: "23h 16m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
+        year: "1998",
+        brand: "Lexus",
+        model: "IS 300",
+        mileage: "10,590",
+        price: "9,790",
+        location: "San Antonio, TX 78154",
+        timeLeft: "10h 53m",
+    },
+];
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,41 +1,5 @@
 import "../assets/Cards.css";
-
-// Example data
-const cars = [
-    {
-        imageURL:
-            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
-        year: "1998",
-        brand: "Lexus",
-        model: "IS 300",
-        mileage: "10,590",
-        price: "9,790",
-        location: "San Antonio, TX 78154",
-        timeLeft: "10h 53m",
-    },
-    {
-        imageURL:
-            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
-        year: "2000",
-        brand: "Ford",
-        model: "F-350 Super Duty Lariat 4x4",
-        mileage: "87,700",
-        price: "12,500",
-        location: "Loveland, OH 45140",
-        timeLeft: "00h 1m",
-    },
-    {
-        imageURL:
-            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
-        year: "2017",
-        brand: "Ford",
-        model: "GT350 Fastback RWD",
-        mileage: "32,900",
-        price: "62,000",
-        location: "Fort Gratiot, MI 48059",
-        timeLeft: "23h 16m",
-    },
-];
+import { cars } from "../assets/sample_data";
 
 function Card({
     imageURL,
@@ -58,7 +22,7 @@ function Card({
         carName = carName.concat("...");
     }
     return (
-        <div className="card">
+        <a href="" className="card">
             <div
                 className="card-image"
                 style={{ backgroundImage: `url(${imageURL})` }}
@@ -78,16 +42,20 @@ function Card({
                     </ul>
                 </div>
             </div>
-        </div>
+        </a>
     );
 }
 
 const Cards = () => {
+    // change key soon
     return (
-        <div className="cards-container">
-            {cars.map((car) => (
-                <Card {...car} key={car.price} />
-            ))}
+        <div className="cards-flexbox">
+            <div className="cards-container">
+                {" "}
+                {cars.map((car) => (
+                    <Card {...car} key={car.price} />
+                ))}
+            </div>
         </div>
     );
 };
