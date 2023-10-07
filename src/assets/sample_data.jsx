a5a550e4abb772d143e36d271369322329ad5970
klupka <sethklupka123@gmail.com>
2023-10-07 13:31:55 -0500

    changed some styling.

--- src/assets/sample_data.jsx
+++ src/assets/sample_data.jsx
@@ -12,36 +12,14 @@ export const cars = [
     },
     {
         imageURL:
-            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
-        year: "2000",
-        brand: "Ford",
-        model: "F-350 Super Duty Lariat 4x4",
-        mileage: "87,700",
-        price: "12,500",
-        location: "Loveland, OH 45140",
-        timeLeft: "00h 1m",
-    },
-    {
-        imageURL:
-            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
-        year: "2017",
-        brand: "Ford",
-        model: "GT350 Fastback RWD",
-        mileage: "32,900",
-        price: "44,000",
-        location: "Fort Gratiot, MI 48059",
-        timeLeft: "23h 16m",
-    },
-    {
-        imageURL:
-            "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
-        year: "1998",
-        brand: "Lexus",
-        model: "IS 300",
-        mileage: "10,590",
-        price: "9,790",
-        location: "San Antonio, TX 78154",
-        timeLeft: "10h 53m",
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/ed21d7e657764d8811eba1ee4364bb7daf73f710/photos/3gNdNaNz-senOisfQdk.jpg?t=169411902178",
+        year: "2019",
+        brand: "Chevrolet",
+        model: "Camaro ZL1 Coupe",
+        mileage: "34,100",
+        price: "68,200",
+        location: "Austin, TX 78749",
+        timeLeft: "49h 592m",
     },
     {
         imageURL:
@@ -56,6 +34,28 @@ export const cars = [
     },
     {
         imageURL:
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/da6e8a1d4896127a9d2adf83461993b45339474c/photos/KPb4glB7-tUJ9fbXDiv-v0ATRQYoZa.jpg?t=169611611966",
+        year: "1990",
+        brand: "Honda",
+        model: "Prelude Si 4WS",
+        mileage: "109,330",
+        price: "14,290",
+        location: "Springfield, OR 35144",
+        timeLeft: "16h 03m",
+    },
+    {
+        imageURL:
+            "https://www.selectjeeps.com/imagetag/13247/main/l/Used-2006-Jeep-Wrangler-Unlimited-Rubicon.jpg",
+        year: "2006",
+        brand: "Jeep",
+        model: "Wrangler Rubicon 4x4",
+        mileage: "57,900",
+        price: "19,600",
+        location: "Henderson, NV 89012",
+        timeLeft: "76h 22m",
+    },
+    {
+        imageURL:
             "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
         year: "2017",
         brand: "Ford",
@@ -67,6 +67,17 @@ export const cars = [
     },
     {
         imageURL:
+            "https://d2q97jj8nilsnk.cloudfront.net/images/PXL_20230207_202103350.PORTRAIT.jpg",
+        year: "2022",
+        brand: "Rivian",
+        model: "R1T Adventure Edition",
+        mileage: "7,900",
+        price: "32,900",
+        location: "Apex, NC 27539",
+        timeLeft: "03h 55m",
+    },
+    {
+        imageURL:
             "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
         year: "1998",
         brand: "Lexus",
@@ -75,6 +86,17 @@ export const cars = [
         price: "9,790",
         location: "San Antonio, TX 78154",
         timeLeft: "10h 53m",
+    },
+    {
+        imageURL:
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/ed21d7e657764d8811eba1ee4364bb7daf73f710/photos/3gNdNaNz-senOisfQdk.jpg?t=169411902178",
+        year: "2019",
+        brand: "Chevrolet",
+        model: "Camaro ZL1 Coupe",
+        mileage: "34,100",
+        price: "68,200",
+        location: "Austin, TX 78749",
+        timeLeft: "49h 592m",
     },
     {
         imageURL:
@@ -89,6 +111,28 @@ export const cars = [
     },
     {
         imageURL:
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/da6e8a1d4896127a9d2adf83461993b45339474c/photos/KPb4glB7-tUJ9fbXDiv-v0ATRQYoZa.jpg?t=169611611966",
+        year: "1990",
+        brand: "Honda",
+        model: "Prelude Si 4WS",
+        mileage: "109,330",
+        price: "14,290",
+        location: "Springfield, OR 35144",
+        timeLeft: "16h 03m",
+    },
+    {
+        imageURL:
+            "https://www.selectjeeps.com/imagetag/13247/main/l/Used-2006-Jeep-Wrangler-Unlimited-Rubicon.jpg",
+        year: "2006",
+        brand: "Jeep",
+        model: "Wrangler Rubicon 4x4",
+        mileage: "57,900",
+        price: "19,600",
+        location: "Henderson, NV 89012",
+        timeLeft: "76h 22m",
+    },
+    {
+        imageURL:
             "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
         year: "2017",
         brand: "Ford",
@@ -100,6 +144,17 @@ export const cars = [
     },
     {
         imageURL:
+            "https://d2q97jj8nilsnk.cloudfront.net/images/PXL_20230207_202103350.PORTRAIT.jpg",
+        year: "2022",
+        brand: "Rivian",
+        model: "R1T Adventure Edition",
+        mileage: "7,900",
+        price: "32,900",
+        location: "Apex, NC 27539",
+        timeLeft: "03h 55m",
+    },
+    {
+        imageURL:
             "https://bringatrailer.com/wp-content/uploads/2020/01/2003_lexus_is_15820631675ef66e7dff9f98764d2003_lexus_is_1582063166fcd208c652a0e1-af90-4ead-b2ba-431400d2a900-lFawgq.jpg?fit=940%2C627",
         year: "1998",
         brand: "Lexus",
@@ -108,5 +163,71 @@ export const cars = [
         price: "9,790",
         location: "San Antonio, TX 78154",
         timeLeft: "10h 53m",
+    },
+    {
+        imageURL:
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/ed21d7e657764d8811eba1ee4364bb7daf73f710/photos/3gNdNaNz-senOisfQdk.jpg?t=169411902178",
+        year: "2019",
+        brand: "Chevrolet",
+        model: "Camaro ZL1 Coupe",
+        mileage: "34,100",
+        price: "68,200",
+        location: "Austin, TX 78749",
+        timeLeft: "49h 592m",
+    },
+    {
+        imageURL:
+            "https://bringatrailer.com/wp-content/uploads/2022/07/2000_ford_f-350_2000_ford_f-350_9fe63c46-6247-437d-80e4-bbf3097df9a5-lebyz8-23953-88180.jpg",
+        year: "2000",
+        brand: "Ford",
+        model: "F-350 Super Duty Lariat 4x4",
+        mileage: "87,700",
+        price: "12,500",
+        location: "Loveland, OH 45140",
+        timeLeft: "00h 1m",
+    },
+    {
+        imageURL:
+            "https://media.carsandbids.com/cdn-cgi/image/width=2080,quality=80/da6e8a1d4896127a9d2adf83461993b45339474c/photos/KPb4glB7-tUJ9fbXDiv-v0ATRQYoZa.jpg?t=169611611966",
+        year: "1990",
+        brand: "Honda",
+        model: "Prelude Si 4WS",
+        mileage: "109,330",
+        price: "14,290",
+        location: "Springfield, OR 35144",
+        timeLeft: "16h 03m",
+    },
+    {
+        imageURL:
+            "https://www.selectjeeps.com/imagetag/13247/main/l/Used-2006-Jeep-Wrangler-Unlimited-Rubicon.jpg",
+        year: "2006",
+        brand: "Jeep",
+        model: "Wrangler Rubicon 4x4",
+        mileage: "57,900",
+        price: "19,600",
+        location: "Henderson, NV 89012",
+        timeLeft: "76h 22m",
+    },
+    {
+        imageURL:
+            "https://www.mustangspecs.com/wp-content/uploads/2022/07/IMG_0944.jpg",
+        year: "2017",
+        brand: "Ford",
+        model: "GT350 Fastback RWD",
+        mileage: "32,900",
+        price: "44,000",
+        location: "Fort Gratiot, MI 48059",
+        timeLeft: "23h 16m",
+    },
+    {
+        imageURL:
+            "https://d2q97jj8nilsnk.cloudfront.net/images/PXL_20230207_202103350.PORTRAIT.jpg",
+        year: "2022",
+        brand: "Rivian",
+        model: "R1T Adventure Edition",
+        mileage: "7,900",
+        price: "32,900",
+        location: "Apex, NC 27539",
+        timeLeft: "03h 55m",
     },
 ];
